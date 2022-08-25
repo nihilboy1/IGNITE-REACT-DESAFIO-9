d6abe95c13c359357b577a79660277bbd2aa7cab
Samuel Seve <samuelseve1@gmail.com>
2022-08-25 14:41:20 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], // pastas a serem ignoradas durante os testes
+  testEnvironment: 'jsdom', // indicação de modelo de ambiente que o jest deve esperar
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'], // pasta que deve rodar antes de todos os testes
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  }, // extensões de arquivos que devem ser transformadas durante o processo
+  moduleNameMapper: {
+    '\\.(scss|css|sass)$': 'identity-obj-proxy'
+  }, // serve para fazer parse dos módulos de estilo importados dentro dos componentes
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec. tsx',
+    '!src/**/_app.tsx',
+    '!src/**/_app.tsx'
+  ],
+  coverageReporters: ['lcov', 'json']
+}
